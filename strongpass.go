2700249ca3c710aab154bc82110a1cf0869686ed
Rylan <rylans@gmail.com>
2018-05-07 11:36:30 -0700

    Add common passwords rule

--- strongpass.go
+++ strongpass.go
@@ -1,26 +1,60 @@
-// Package strongpass provides password strength validation 
+// Package strongpass provides password strength validation
 package strongpass
 
+import (
+	"strings"
+)
+
+type CheckRule func(string) string
+
 type ValidationRule struct {
+	description string
+	check       CheckRule
 }
 
 type Validator struct {
-  rules []ValidationRule
+	rules []ValidationRule
 }
 
 type ValidationResult struct {
-  strength int
-  warnings []string
-  errors []string
+	strength int
+	warnings []string
+	errors   []string
 }
 
 func NewValidator() *Validator {
-  return &Validator{}
+	return &Validator{rules: make([]ValidationRule, 0)}
 }
 
-func (validator *Validator) Validate(pw string) (ValidationResult) {
-  result := ValidationResult{}
+func (validator *Validator) Validate(pw string) ValidationResult {
+	result := ValidationResult{errors: make([]string, 0)}
 
-  return result
+	for _, rule := range validator.rules {
+		errorMsg := rule.check(pw)
+		if errorMsg != "" {
+			result.errors = append(result.errors, errorMsg)
+		}
+	}
+
+	return result
 }
 
+func (validator *Validator) NoCommonPasswords() {
+	validator.rules = append(validator.rules, newCommonPasswordsRule())
+}
+
+func newCommonPasswordsRule() ValidationRule {
+	rule := ValidationRule{}
+	rule.description = "Your password contains a commonly used password."
+	rule.check = func(pw string) string {
+		commonPws := []string{"root", "master", "1234", "letmein",
+			"password", "qwerty", "admin", "shadow", "hello", "password1", "trustno1"}
+		for _, commonPw := range commonPws {
+			if strings.Contains(pw, commonPw) {
+				return "Password contains string '" + commonPw + "'"
+			}
+		}
+		return ""
+	}
+	return rule
+}
--- strongpass_test.go
+++ strongpass_test.go
@@ -6,8 +6,17 @@ import (
 )
 
 func TestZeroValidationRules(t *testing.T) {
-  validator := NewValidator()
+	validator := NewValidator()
 
-  result := validator.Validate("abc123")
-  assert.Equal(t, 0, result.strength)
+	result := validator.Validate("abc123")
+	assert.Equal(t, 0, result.strength)
+}
+
+func TestCommonPasswordsRule(t *testing.T) {
+	validator := NewValidator()
+	validator.NoCommonPasswords()
+
+	assert.Equal(t, "Password contains string 'letmein'", validator.Validate("letmein").errors[0])
+	assert.Equal(t, "Password contains string 'password'", validator.Validate("password").errors[0])
+	assert.Equal(t, "Password contains string 'hello'", validator.Validate("hello").errors[0])
 }
